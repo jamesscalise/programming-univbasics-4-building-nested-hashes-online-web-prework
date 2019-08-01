af0a10bde283bf96eebc14f7a5a11a80db3ae054
James Scalise <jamescscalise@gmail.com>
2019-08-01 05:57:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,13 @@ end
 
 def monopoly_with_second_tier
 	monopoly =  {
-  :railroads=>{
+  :railroads=>
+  {
     :pieces=>4,
-    :names=>{}
-    :rent_in_dollars=>{},
+    :rent_in_dollars=>
+    {},
+    :names=>
+    {}
   }
 }
 
